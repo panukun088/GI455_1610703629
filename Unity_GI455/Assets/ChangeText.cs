0fc8af23b1ccd36ee2a3198d884e54517c191cfd
unknown <Phanukun.chai@bumail.net>
2021-01-26 23:21:21 +0700

    Send assignment Week1

---
+++ Unity_GI455/Assets/ChangeText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text TextChange = GameObject.Find("Canvas/Text").GetComponent<Text>();
+        TextChange.text = "Fubarrr";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity_GI455/Assets/Finder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Finder : MonoBehaviour
+{
+    private GameObject Find;
+    public Button btnClick;
+    public InputField InputText;
+    void Start()
+    {
+        btnClick.onClick.AddListener(GetInPutOnClickHandler);
+        
+       
+    }
+
+    // Update is called once per frame
+    
+    public void GetInPutOnClickHandler()
+    {
+
+        if (Find = GameObject.Find(InputText.text))
+        {
+            Debug.Log("Found"+InputText.text);
+            Text TextChange = GameObject.Find("Canvas/Button/Text").GetComponent<Text>();
+            TextChange.text = InputText.text +" is found";
+
+        }
+        else if(Find == null)
+        {
+            Debug.Log("Can't Find"+InputText.text);
+            Text TextChange = GameObject.Find("Canvas/Button/Text").GetComponent<Text>();
+            TextChange.text = InputText.text + " is not found";
+        }
+    }
+
+    public void SearchResult()
+    {
+       
+
+    }
+}
